d6a7b853eb3bf4e46dc7267d3eab2024d11f2683
jaemanii <jaemanii@localhost>
2020-11-22 03:23:37 +0900

    pom.xml 정리

--- src/main/java/com/myassign/ApiApplication.java
+++ src/main/java/com/myassign/ApiApplication.java
@@ -2,7 +2,6 @@ package com.myassign;
 
 import java.util.Arrays;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @ComponentScan("com.myassign")
 public class ApiApplication {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
